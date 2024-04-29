5bb8ab65cb578dd707db742c8a17449e8af3130a
capptt <106069231+captainthx@users.noreply.github.com>
2024-04-29 17:29:03 +0700

    Update Pagination.java

--- ex-common/src/main/java/com/yotsuki/excommon/model/Pagination.java
+++ ex-common/src/main/java/com/yotsuki/excommon/model/Pagination.java
@@ -1,11 +1,17 @@
 package com.yotsuki.excommon.model;
 
-import lombok.Data;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
-@Data
-public class Pagination {
-    private Integer page = 0;
-    private Integer limit = 10;
-    private String sortType = "asc";
-    private String sortBy = "id";
+public class Pagination extends PageRequest {
+    public static final int DEFAULT_PAGE = 0;
+    public static final int DEFAULT_LIMIT = 10;
+    public static Sort DEFAULT_SORT = Sort.unsorted();
+
+    public Pagination(Integer page, Integer size, String sort) {
+        super(page != null && page > 0 ? page - 1 : DEFAULT_PAGE,
+                size != null && size > 0 ? size : DEFAULT_LIMIT,
+                sort != null ? Sort.by(Sort.Direction.fromString(sort.split(",")[1]),sort.split(",")[0]) :DEFAULT_SORT);
+    }
+
 }
